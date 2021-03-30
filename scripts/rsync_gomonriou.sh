f69771af1be6d0e93ac3040ced26cda3306340b0
Arnaud Boyé <arnaud.boye2@gmail.com>
2021-03-30 12:04:25 +0200

    update rsync

--- scripts/rsync_gomonriou.sh
+++ scripts/rsync_gomonriou.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 rsync --verbose -e "ssh -p 22" --recursive --update --archive --compress  --omit-dir-times gomonriou@10.6.0.2:/home/shared/ /home/shared/
 rsync --verbose -e "ssh -p 22" --recursive --update --archive --compress  --omit-dir-times /home/shared/ gomonriou@10.6.0.2:/home/shared/
--- scripts/rsync_sneaky.sh
+++ scripts/rsync_sneaky.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 rsync --verbose -e "ssh -p 22" --recursive --update --archive --compress  --omit-dir-times sneaky@10.6.0.1:/home/shared/ /home/shared/
 rsync --verbose -e "ssh -p 22" --recursive --update --archive --compress  --omit-dir-times /home/shared/ sneaky@10.6.0.1:/home/shared/
