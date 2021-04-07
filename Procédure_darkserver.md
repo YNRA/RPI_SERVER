207e04708a0ff703f6d596b8398864cd53d17f23
Gomonriou <thomas.dejeanne@hotmail.fr>
2021-04-07 11:15:47 +0200

    ADD procedure_darkserver

---
+++ Procédure_darkserver.md
@@ -0,0 +1,21 @@
+apt-get install tor
+mkdir /var/www/darkserver
+mettre l'html dedans
+python3 -m http.server --bind 127.0.0.1 <port>
+
+dans /etc/tor/torrc l71 decomenter et changer le port
+HiddenServiceDir /var/lib/tor/hidden_service/
+HiddenServicePort 80 127.0.0.1:5631
+HiddenServicePort 443 127.0.0.1:5631
+
+redemarrer tor (sudo service tor restart )
+cat /var/lib/tor/hidden_service/hostname
+ztbq2emkuuulqykwn43cj5il3w7x3xk5b5dzcqahiedwz6t5svdv4sqd.onion
+
+SI ON PEUT INSTALLER UPOG
+<!-- sudo apt update
+sudo apt install -y python3-pip
+pip3 install updog
+export PATH=$PATH:/home/kali/.local/bin 
+LANCER çA a la place du python 
+updog -p 5631 --ssl-->
--- scripts/wireguard.sh
+++ scripts/wireguard.sh
@@ -3,3 +3,35 @@ curl -L https://install.pivpn.io | bash
 # Add a user : pivpn -a 
 # Enable or disable wg : wg-quick up/down INTERFACE
 # conf is copied in /home/user/configs ; copy and transfer to define user
+
+
+# HOTE 
+[Interface]
+PrivateKey = eA0GLDZ/+rnD9DOSyt/GDNRPr9EDDQlQ3X1cFQGMxGs=
+Address = 10.6.0.4/29
+DNS = 1.1.1.1, 8.8.8.8
+
+[Peer]
+PublicKey = 0G37Umm89r00hEn9oI5a4/fmeqlBcgHW61K+ecAN9Wg=
+PresharedKey = ob1D5eW6Dw7R7hpsVTnLhtlxyo7GpLvUpGiYWfJPCNA=
+Endpoint = 90.120.170.212:51330
+AllowedIPs = 10.6.0.1/32
+
+
+# RPI 
+[Interface]
+Address = 10.6.0.1/29
+ListenPort = 51330
+PrivateKey = WEow/EnyBDJ4zmyk3Yn7WDWivruKfbYHsqdv+nfPt2I=
+
+[Peer]
+PublicKey = xtvu53T/hWltoyJhWDwKpAJhoxJKd+I8OMZQqAucWiQ=
+PresharedKey = N8ro0hnHiCANsP8zxxi6eYZMLF5ZT+Sw5nUx9ABT1CE=
+AllowedIPs = 10.6.0.2/32
+Endpoint = 86.215.12.125:51330
+### begin gomonriou_ubuntu ###
+[Peer]
+PublicKey = UCnx4CjKIF7yc853KPAxvUaZIp8hpgRzcNCpSMSaglY=
+PresharedKey = ob1D5eW6Dw7R7hpsVTnLhtlxyo7GpLvUpGiYWfJPCNA=
+AllowedIPs = 10.6.0.4/32
+### end gomonriou_ubuntu ###
