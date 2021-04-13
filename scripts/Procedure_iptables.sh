08120c2c968b03c574b8574f97c7e3e333225de4
Arnaud Boyé <arnaud.boye2@gmail.com>
2021-04-13 15:32:25 +0200

    Iptables

--- scripts/Procedure_iptables.sh
+++ scripts/Procedure_iptables.sh
@@ -1,11 +1,9 @@
 echo "Applying rules, starting firewall ..."
 sleep 1
 
-# FLUSH RULES & START FIREWALL
+# FLUSH RULES
 iptables -t filter -F
 iptables -t filter -X
-systemctl enable firewalld
-systemctl start firewalld
 
 # STRICT POLICY
 iptables -t filter -P INPUT DROP
@@ -23,7 +21,7 @@ iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
 iptables -A INPUT -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
 iptables -A FORWARD -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s -j ACCEPT
 
-# ACCEPT ICMP FROM VPN NETWORK
+# ACCEPT FROM VPN NETWORK
 iptables -A INPUT -p icmp -s 10.6.0.0/29 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 
 # ACCEPT LOOPBACK, DROP TRAFFIC TO 127.0 THAT DOESN'T USE LO
@@ -39,13 +37,25 @@ iptables -A INPUT -p tcp --dport 443 -m state --state NEW -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -m state --state NEW -j ACCEPT
 iptables -A INPUT -p tcp --dport 53 -m state --state NEW -j ACCEPT
 iptables -A INPUT -p udp --dport 53 -m state --state NEW -j ACCEPT
+iptables -A INPUT -p tcp --dport 51330 -m state --state NEW, ESTABLISHED, RELATED -j ACCEPT
 
 # VPN ROUTE
-iptables -t nat -A POSTROUTING -o wlan0 -m comment --comment wireguard-nat-rule -j MASQUERADE
-iptables -t nat -A POSTROUTING -o eth0 -m comment --comment wireguard-nat-rule -j MASQUERADE
+# NAT RULES
+iptables -t nat -A POSTROUTING 1 -s 10.6.0.0/29 -o wlan0 -j MASQUERADE
+iptables -t nat -A POSTROUTING 1 -s 10.6.0.0/29 -o eth0 -j MASQUERADE
+# ACCEPT TRAFFIC CREATED BY WG0 INTERFACE
+iptables -A INPUT -i wg0 -j accept
+# ALLOW PACKET BEING ROUTED THROUGH WG SERVER 
+iptables -A FORWARD 1 -i eth0 -o wg0 -j ACCEPT
+iptables -A FORWARD 1 -i wlan0 -o wg0 -j ACCEPT
+iptables -A FORWARD 1 -i wg0 -o eth0 -j ACCEPT
+iptables -A FORWARD 1 -i wg0 -o wlan0 -j ACCEPT
 
 # LOG DENIED IP
 iptables -A INPUT -m limit --limit 5/min -j LOG --log-prefix "IPTABLES DENIED: " --log-level 7
+
+# TURN ON IP FORWARDING
+sysctl -w net.ipv4.ip_forward=1
 
 echo "Done ! Rules applied : "
 sleep 1
