c7efe4e1f60c3ff87b6b56a4bdc4f312bdfa9b96
YNRA <62951468+YNRA@users.noreply.github.com>
2021-04-28 14:59:30 +0200

    Delete scripts directory

--- scripts/rsync_gomonriou.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sleep 2
-rsync --verbose -e "ssh -p 22" --recursive --update --archive --compress --times --delete  /home/shared/ gomonriou@10.6.0.2:/home/shared/
-rsync --verbose -e "ssh -p 22" --recursive --update --archive --compress --times --delete gomonriou@10.6.0.2:/home/shared/ /home/shared/
